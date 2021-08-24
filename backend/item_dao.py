612f771c6fea3b3a0f487e367c1fd7edddd271f2
Jddumas <jacobdmdumas@gmail.com>
2021-08-24 14:16:10 -0500

    add basic backend structure

---
+++ backend/item_dao.py
@@ -0,0 +1,20 @@
+class ItemDAO:
+    def __init__(self):
+        print("Instantiating Item DAO")
+    
+    def get():
+        pass
+    
+    def create():
+        pass
+    
+    def patch():
+        pass
+    
+    def ship():
+        pass
+    
+    def expire():
+        pass
+    
+item_dao = ItemDAO()
---
+++ backend/product_dao.py
@@ -0,0 +1,21 @@
+class ProductDAO:
+    def __init__(self):
+        print("Instantiating product dao")
+        return self
+
+    def get(sku):
+        pass
+    
+    def create(product_configs):
+        pass
+    
+    def update(sku, product_configs):
+        pass
+    
+    def deactivate(sku):
+        pass
+
+    def activate(sku):
+        pass
+
+product_dao = ProductDAO()
