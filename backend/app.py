78f82690e7b39dcfda8d8371698675daa86b7c69
Jddumas <jacobdmdumas@gmail.com>
2021-08-26 13:39:20 -0500

    updated crud

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,70 @@
-from flask import Flask
+from flask import Flask, abort
+from product_dao import product_dao
 
 app = Flask(__name__)
 
 @app.route("/")
 def default():
-    pass
+    return "Hello world"
+
+@app.route("/products")
+def get_all_products():
+    return product_dao.get_all()
+
+@app.route("/products/<sku>")
+def get_products_by_sku(sku):
+    product = product_dao.get(sku)
+    
+    if product is None:
+        abort(404)
+    else:
+        return product
+
+# create
+@app.route("/products", methods = ['POST'])
+def create_product():
+    new_product = req.body
+    # new_product = {
+    #             "6"{
+    #                'sku': "6",
+    #                'description': "something",
+    #                'brand': "fdsafdsafds",
+    #                'price': 43,
+    #                'weight': 34,
+    #                'category': "lala",
+    #                'stock': 13,
+    #                'thumbnail_url': "www.fdsfd.com",
+    #                'shelf_life': 45,
+    #                'active': True
+    #                 }
+    #             }
+    product = product_dao.create(new_product)
+    return product
+
+# update
+@app.route("/products/<sku>", methods = ['PUT'])
+def update_product():
+    product = update(sku, product_configs)
+    return product
+
+
+# deactivate/activate
+@app.route('/products/<sku>', methods = ['PUT'])
+def deactivate_product_by_sku(sku):
+    # if the product does not exist
+    product = product_dao.get(sku)    
+    if product is None:
+        abort(404)
+    
+    # if the product does exist
+    else:
+        if product.active == True:
+            product = product_dao.deactivate(sku)
+            return ("deactivated", product)
+        else:
+            product = product_dao.activate(sku)
+            return ("activated", product)
+    
+
+
+
--- backend/product_dao.py
+++ backend/product_dao.py
@@ -1,21 +1,65 @@
+mocks = {
+    '1': {
+        'sku': 1,
+        'description': 'Peach box from Federiksburg, Texas',
+        'brand': 'All Nature',
+        'price': 3.00,
+        'weight': 1.00,
+        'category': 'fruits',
+        'stock': 100,
+        'thumbnail_url': 'https://assets.wsimgs.com/wsimgs/rk/images/dp/wcm/202130/0050/img23o.jpg',
+        'shelf_life': 7,
+        'active': True
+    },
+    '2': {
+        'sku': 2,
+        'description': 'Spaghetti box from Italy',
+        'brand': 'Skinner',
+        'price': 5.00,
+        'weight': 3.00,
+        'category': 'Pasta',
+        'stock': 200,
+        'thumbnail_url': 'https://i5.walmartimages.com/asr/bb8887af-420d-471d-a9db-e18010f6f369_1.225db54c3a0c677cb668f90b10b9737c.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF',
+        'shelf_life': 120,
+        'active': True
+    },
+    '3': {
+        'sku': 3,
+        'description': 'Potatos from Idaho',
+        'brand': 'TaterLand',
+        'price':6.00,
+        'weight': 10.00,
+        'category': 'Vegetable',
+        'stock': 50,
+        'thumbnail_url': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.instacart.com%2Fproducts%2F16725303-russet-potato-bag-10-lbs&psig=AOvVaw0xsfbRvGRYi7JNVr3IbC-N&ust=1629919947007000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKj7tI-zyvICFQAAAAAdAAAAABAI',
+        'shelf_life': 14,
+        'active': True
+        }
+}
+
 class ProductDAO:
     def __init__(self):
         print("Instantiating product dao")
-        return self
 
-    def get(sku):
-        pass
+    def get_all(self):
+        return mocks
+
+    def get(self, sku):
+        return mocks.get(sku)        
     
     def create(product_configs):
-        pass
+        mocks.append(product_configs)
+        return mocks
     
     def update(sku, product_configs):
         pass
     
     def deactivate(sku):
-        pass
+        product = product_dao.get(sku)
+        product.active = False
 
     def activate(sku):
-        pass
+        product = product_dao.get(sku)
+        product.active = True
 
 product_dao = ProductDAO()
